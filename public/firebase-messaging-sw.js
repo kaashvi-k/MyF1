f38d3960a9eabbb7735a30043e561767f1a69a94
Kaashvi <kaashvikakkar@gmail.com>
2026-06-25 23:39:24 +0530

    notification fixing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,28 @@
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.15.0/firebase-app-compat.js"
+);
 
-firebase.initializeApp({
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging-compat.js"
+);
+
+const firebaseConfig = {
   apiKey: "AIzaSyAavGJxy4qVrRiGesBPhbqdk8GJEVNGBcU",
   authDomain: "myf1-e46f1.firebaseapp.com",
   projectId: "myf1-e46f1",
   storageBucket: "myf1-e46f1.firebasestorage.app",
   messagingSenderId: "495745229566",
   appId: "1:495745229566:web:c2b097ebcafee0a8e3fbaf"
-});
+};
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-  });
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/favicon.ico",
+    }
+  );
 });
