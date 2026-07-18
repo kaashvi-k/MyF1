6efb938fb421b4b2cc98d1683b066d0ba2654916
Kaashvi <kaashvikakkar@gmail.com>
2026-07-19 00:17:27 +0530

    testing email

--- api/send-email-reminders.js
+++ api/send-email-reminders.js
@@ -1,19 +1,47 @@
-import { resend } from "./resend.js";
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getMessaging } from 'firebase-admin/messaging';
+import { Resend } from "resend";
+
+
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+}
+
+const db = getFirestore();
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   try {
-    const data = await resend.emails.send({
-      from: "ApexF1 <onboarding@resend.dev>",
-      to: ["kaashvikakkar@gmail.com"],
-      subject: "Testing Resend",
-      html: "<h1>Hello!</h1>",
+    const usersSnap = await db.collection("users").get();
+
+    const emails = [];
+
+    usersSnap.forEach(doc => {
+      const user = doc.data();
+
+      if (user.email) {
+        emails.push(user.email);
+      }
     });
 
-    console.log(data);
+    return res.status(200).json({
+      emails,
+    });
 
-    res.status(200).json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json(err);
+
+    return res.status(500).json({
+      error: err.message,
+    });
   }
 }
+
